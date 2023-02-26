d5381a555a9a46844cc1e487a95da8a3539ab6f2
Bruno Luiz <sitespomerode@gmail.com>
2023-02-26 10:52:06 -0300

    Add offset and limit to queries/secure function

--- includes/functions.php
+++ includes/functions.php
@@ -13,9 +13,29 @@ function page_footer($page)
     $smarty->display("$page.tpl");
 }
 
+/* ------------------------------- */
+/* Price Format */
+/* ------------------------------- */
+
+/**
+ * secure
+ *
+ * @param number $value
+ */
+
 function price_format($price){
 	return number_format($price,2,",",".");
 }
+
+/* ------------------------------- */
+/* Description */
+/* ------------------------------- */
+
+/**
+ * secure
+ *
+ * @param string $value
+ */
 
 function description($description){
 
@@ -24,4 +44,50 @@ function description($description){
 	}
 	return $description;
 
+}
+
+/* ------------------------------- */
+/* Security */
+/* ------------------------------- */
+
+/**
+ * secure
+ *
+ * @param string $value
+ * @param string $type
+ * @param boolean $quoted
+ * @return string
+ */
+function secure($value, $type = "", $quoted = true)
+{
+    global $db;
+    if ($value !== 'null' or $value !== '') {
+        // [1] Sanitize
+        /* Escape all (single-quote, double quote, backslash, NULs) */
+        if (@get_magic_quotes_gpc()) {
+            $value = stripslashes($value);
+        }
+        /* Convert all applicable characters to HTML entities */
+        $value = @htmlentities($value, ENT_QUOTES, 'utf-8');
+        // [2] Safe SQL
+        $value = $db->real_escape_string($value);
+        switch ($type) {
+            case 'int':
+            $value = ($quoted) ? "'" . intval($value) . "'" : intval($value);
+            break;
+            case 'datetime':
+            $value = ($quoted) ? "'" . set_datetime($value) . "'" : set_datetime($value);
+            break;
+            case 'search':
+            $value = (!is_empty($value)) ? "'%" . $value . "%'" : "''";
+            break;  
+            case 'search_tag':
+            $value = (!is_empty($value)) ? "'+" . $value . "'" : "''";
+            break;
+            default:
+            $value = (!is_empty($value)) ? "'" . $value . "'" : "''";
+            break;
+        }
+    }
+    return $value;
 }

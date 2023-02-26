b3c4fece3c2b76c1a9cac1064aac57c713f629b3
Bruno Luiz <sitespomerode@gmail.com>
2023-02-26 19:18:22 -0300

    Adicionando botão de excluir produtos

--- content/themes/default/templates/products.tpl
+++ content/themes/default/templates/products.tpl
@@ -8,24 +8,25 @@
         <th scope="col">Preço</th>
         <th scope="col">Estoque</th>
         <th scope="col">Imagem</th>
-        <th scope="col">Edit</th>
+        <th scope="col">Ações</th>
       </tr>
     </thead>
     <tbody class='js_products_table'>
       {foreach $products as $product}
-        <tr>
-        <th scope="row">{$product['id']}</th>
-        <td>{$product['name']}</td>
-        <td>{$product['description']}</td>
-        <td>R${$product['price']}</td>
-        <td>{$product['stock']}</td>
-        <td>
-          <img class="small_image_table" src="{$product['image']}">
-        </td>
-        <td>
-          <a href="{{$system['system_url']}}/edit/product/{{$product['id']}}"><i class="fa fa-pencil"></i></a>
-        </td>
-      </tr>
+        <tr class='line_product hide_product{$product['id']}'>
+          <th scope="row">{{$product['id']}}</th>
+          <td>{$product['name']}</td>
+          <td>{$product['description']}</td>
+          <td>R${$product['price']}</td>
+          <td>{$product['stock']}</td>
+          <td>
+            <img class="small_image_table" src="{$product['image']}">
+          </td>
+          <td>
+            <a class='btn' href="{{$system['system_url']}}/edit/product/{{$product['id']}}"><i class="fa fa-pencil text-primary"></i></a>
+            <button class='btn js_delete_product' data-id="{{$product['id']}}"><i class="fa fa-trash text-danger"></i></button>
+          </td>
+        </tr>
       {/foreach}
     </tbody>
   </table>
--- content/themes/default/templates_compiled/2a82c49ea6faeaeeb63d7686b9cf2216b3e74c72_0.file.modals.tpl.php
+++ content/themes/default/templates_compiled/2a82c49ea6faeaeeb63d7686b9cf2216b3e74c72_0.file.modals.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2023-02-26 17:09:20
+/* Smarty version 3.1.33, created on 2023-02-26 23:04:36
   from '/Applications/XAMPP/xamppfiles/htdocs/testePHP/content/themes/default/templates/modals.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_63fb84307c0927_49070193',
+  'unifunc' => 'content_63fbd774d5ede3_10369706',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2a82c49ea6faeaeeb63d7686b9cf2216b3e74c72' => 
     array (
       0 => '/Applications/XAMPP/xamppfiles/htdocs/testePHP/content/themes/default/templates/modals.tpl',
-      1 => 1677427757,
+      1 => 1677448857,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63fb84307c0927_49070193 (Smarty_Internal_Template $_smarty_tpl) {
+function content_63fbd774d5ede3_10369706 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- Start Modal create products -->
 <div id="create_product" class="modal class_create_product" tabindex="-1" style="display: none">
   <div class="modal-dialog">
@@ -69,5 +69,27 @@ function content_63fb84307c0927_49070193 (Smarty_Internal_Template $_smarty_tpl)
     </div>
   </div>
 </div>
-<!-- End Modal create products --><?php }
+<!-- End Modal create products -->
+
+<!-- Start Modal create delete tr products -->
+<div class="modal modal_delete_product">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title">Deletar</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+        <div class="modal-body">
+          <h6>Você deseja deletar este produto ?</h6>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+          <button data-id="" type="button" class="btn btn-primary js_btn_delete_product">Deletar</button>
+        </div>
+    </div>
+  </div>
+</div>
+<!-- End Modal create delete tr products --><?php }
 }
